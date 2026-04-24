5eef9d61532d1fcc71d3fb8d3f57ba52dde48671
revaliya80 <revaliya80@gmail.com>
2026-04-24 10:24:10 +0700

    Mahasiswa

---
+++ Mahasiswa24.java
@@ -0,0 +1,16 @@
+public class Mahasiswa24 {
+    String nama, nim, kelas;
+    int nilai;
+Mahasiswa24(){
+    
+}
+Mahasiswa24(String nama, String nim, String kelas) {
+    this.nama = nama;
+    this.nim = nim;
+    this.kelas = kelas;
+    nilai = -1;
+}
+void tugasDinilai (int nilai) {
+    this.nilai = nilai;
+}
+}
